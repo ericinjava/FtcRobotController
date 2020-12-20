11315908d15e122bdfee119a1933ff6bab2fd90e
Eric <erthorndyke@gmail.com>
2020-12-20 15:36:46 -0600

    Created GyroTurn.java Returns to 0 position when moved

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTurn.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+public class GyroTurn extends LinearOpMode {
+
+    //set Motors
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftBack;
+    DcMotor rightBack;
+
+    //set Gyro
+    GyroSensor sensorGyro;
+    ModernRoboticsI2cGyro mrGyro;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //hardware Mapping Motors
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        leftBack = hardwareMap.dcMotor.get("leftBack");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
+
+        //reversing Motors
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //run Using Encoders
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //set Speed Of Turn
+        double turnSpeed = 0.15;
+
+        //hardware Mapping Gyro
+        sensorGyro = hardwareMap.gyroSensor.get("gyro");
+        mrGyro = (ModernRoboticsI2cGyro) sensorGyro;
+
+        //position Variables
+        int zAccumulated;
+        int target = 0;
+
+        //sleep, calibrate, wait for start
+        sleep(1000);
+        mrGyro.calibrate();
+        waitForStart();
+
+        while (mrGyro.isCalibrating()) {
+            zAccumulated = mrGyro.getIntegratedZValue();
+
+            while (Math.abs(zAccumulated) > 3) {
+                if (zAccumulated > 0){
+                    //turn Left
+                    leftFront.setPower(-turnSpeed);
+                    rightFront.setPower(turnSpeed);
+                    leftBack.setPower(-turnSpeed);
+                    rightBack.setPower(turnSpeed);
+                }
+                if (zAccumulated < 0){
+                    //turnRight
+                    leftFront.setPower(turnSpeed);
+                    rightFront.setPower(-turnSpeed);
+                    leftBack.setPower(turnSpeed);
+                    rightBack.setPower(-turnSpeed);
+                }
+
+                zAccumulated = mrGyro.getIntegratedZValue();
+
+                //telemetry update
+                telemetry.addData("1. accu", String.format("803d", zAccumulated));
+            }
+
+            //set Power to 0
+            leftFront.setPower(0);
+            rightFront.setPower(0);
+            leftBack.setPower(0);
+            rightBack.setPower(0);
+
+            //telemetry update and sleep
+            telemetry.addData("1. accu", String.format("803d", zAccumulated));
+            sleep(25);
+        }
+    }
+}
